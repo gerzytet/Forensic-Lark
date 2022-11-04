ea785dcd7cddfe8eb922d2abf05f9bd5af9e79e7
Craig White <gerzytet@gmail.com>
2022-11-04 17:59:04 -0400

    Commit before testing reset functions

--- headquarters/training_area/a_directory/guide.sh
+++ headquarters/training_area/a_directory/guide.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Well done, to continue your training you have to go back to previous directory"
 echo "To do this type in the command cd .."
+echo "1" > "$guide_dir/progress.txt"
--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 if (return 0 2>/dev/null)
 then
     true
@@ -7,9 +9,64 @@ else
     exit 1
 fi
 
-export AGENT_ROOT_DIR="$(pwd)"
-export PATH="$PATH:$AGENT_ROOT_DIR"
-export AGENT_DIR="$AGENT_ROOT_DIR/agent"
-export HQ_DATA_DIR="$AGENT_ROOT_DIR/headquarters"
-cd agent
-hq
+resetProgress() {
+find headquarters -name progress.txt -delete
+}
+
+resetAgent() {
+if [ ! -f "agent.bak" ]
+then
+    echo "agent directory backup not found!"
+    echo "Not resetting"
+    return 1
+fi
+rm -rf agent
+tar -xf agent.bak
+}
+
+if [ $# -eq 1 ]
+then
+    case $1 in
+        reset)
+            echo "This will reset all files in the investigation files in the game to their initial state.  This command will keep some progress intact, although any commands you used to modify files in the game will need to be repeated"
+            echo "If you would like to wipe out all progress as well, use \"source init.sh resetall\""
+            read -p "Really reset? [y/n]: " answer
+            if [ "$answer" = "y" ]
+            then 
+                echo "Resetting..."
+            else
+                echo "Not resetting."
+            fi
+        ;;
+        resetall)
+            echo "This will reset all files in the investigation files in the game to their initial state, as well as all progress."
+            echo "If you would like to attempt to keep progress, use use \"source init.sh reset\""
+            read -p "Really reset? [y/n]: " answer
+            if [ "$answer" = "y" ]
+            then 
+                echo "Resetting..."
+            else
+                echo "Not resetting."
+            fi
+        ;;
+        resetprogress)
+            echo "This will reset all progress, but not any of the agent files.  This is probably not what you want unless you are the developer."
+            read -p "Really reset? [y/n]: " answer
+            if [ "$answer" = "y" ]
+            then
+                echo "Resetting..."
+            else
+                echo "Not resetting."
+            fi
+        ;;
+    esac
+else
+    export AGENT_ROOT_DIR="$(pwd)"
+    export PATH="$PATH:$AGENT_ROOT_DIR"
+    export AGENT_DIR="$AGENT_ROOT_DIR/agent"
+    export HQ_DATA_DIR="$AGENT_ROOT_DIR/headquarters"
+    cd agent
+    hq
+fi
+
+
