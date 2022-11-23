a21649aa3d2ba50fa00fff631491ec30720d4269
dragonvale103 <mlarusso9872@floridapoly.edu>
2022-11-23 13:18:35 -0500

    I have rearraged the training area to better suit the cat and less tutorial, i have also deleted tempfile and error.txt because they were no longer needed

--- headquarters/training_area/a_directory/guide.sh
+++ headquarters/training_area/a_directory/guide.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Well done, to continue your training you have to go back to previous directory"
-echo "To do this type in the command cd .."
+echo "Well done you have entered a new directory, this will be your main form of navigation."
+echo "Next we will teach you about the cat command, the cat command allows you to peer into files."
+echo "Try using cat on the file within this directory, type cat proceeded by the name of the file you wish to look within."
 echo "1" > "$guide_dir/progress.txt"
